7e9b3174fead4294e802d322ec10d8c20ea13d8f
revert099 <winsor_j@outlook.com>
2024-11-02 22:53:58 +0800

    Add files via upload

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
+print(df.columns)
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight'] / (df['height'] / 100) ** 2) > 25
+df['overweight'] = df['overweight'].astype(int)
 
 # 3
-
+df['cholesterol'] = np.where(df['cholesterol'] == 1, 0, 1)
+df['gluc'] = np.where(df['gluc'] == 1, 0, 1)
 
 # 4
 def draw_cat_plot():
+    sns.catplot(data=df)
+    plt.show()
+
     # 5
-    df_cat = None
-
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
     # 6
-    df_cat = None
+    df_cat_counts = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='count')
+    #plt.show()
     
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
+    # 7 & 8
+    catplot = sns.catplot(data=df_cat_counts, x='variable', y='count', hue='value', kind='bar', col='cardio')
+    catplot.set_axis_labels("variable", "total")  # Set y-axis label to "total" as expected by the test
+    fig = catplot.figure
 
     # 9
     fig.savefig('catplot.png')
@@ -38,22 +39,26 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 12))
 
     # 15
-
-
+    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", cmap='coolwarm', square=True, cbar_kws={"shrink": .5}, ax=ax)
+    
 
     # 16
     fig.savefig('heatmap.png')
